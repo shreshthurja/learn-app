813d9897a6853a9cdfb607f3a8287a546ab5dbf7
somendra1998 <somendrasingh1998@gmail.com>
2024-03-07 14:55:00 +0530

    video 13 completed

--- src/App.js
+++ src/App.js
@@ -2,17 +2,31 @@ import "./App.css";
 import Navbar from "./components/Navbar";
 import React, { useState } from "react";
 import TextForm from "./components/TextForm";
+import Alert from "./components/Alert";
 // import About from "./components/About";
 
 function App() {
   const [mode, setMode] = useState("light");
+  const [alert, setAlert] = useState(null);
+
+  const showAlert = (message, type) => {
+    setAlert({
+      msg: message,
+      type: type,
+    });
+    setTimeout(() => {
+      setAlert(null);
+    }, 1500);
+  };
   const themeMode = () => {
     if (mode === "light") {
       setMode("dark");
       document.body.style.backgroundColor = "grey";
+      showAlert("Dark Mode is enabled", "success");
     } else {
       setMode("light");
       document.body.style.backgroundColor = "white";
+      showAlert("Light Mode is enabled", "success");
     }
   };
   return (
@@ -23,8 +37,10 @@ function App() {
         mode={mode}
         themeMode={themeMode}
       />
+      <Alert alert={alert} />
       <div className="container my-3">
         <TextForm
+          showAlert={showAlert}
           heading="Enter the text to analyze"
           mode={mode}
         />
--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -6,15 +6,18 @@ export default function TextForm(props) {
   const handleUppercase = () => {
     let newText = text.toUpperCase();
     setText(newText);
+    props.showAlert("Converted to upper case", "success");
   };
 
   const handleLowercase = () => {
     let newText = text.toLowerCase();
     setText(newText);
+    props.showAlert("Converted to lower case", "success");
   };
 
   const handleClear = () => {
     setText("");
+    props.showAlert("Cleared", "success");
   };
 
   const handleOriginal = () => {
